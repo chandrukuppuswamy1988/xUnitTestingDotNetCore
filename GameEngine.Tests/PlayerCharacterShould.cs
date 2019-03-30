80fca3dc7cc694e47fd2ba9b94c69dd226c80274
Chandru Kuppuswamy <chandrukuppuswamy@outlook.com>
2019-03-30 18:06:47 +0530

    First Tests

---
+++ GameEngine.Tests/PlayerCharacterShould.cs
@@ -0,0 +1,28 @@
+using System;
+using Xunit;
+
+namespace GameEngine.Tests
+{
+    public class PlayerCharacterShould
+    {
+        [Fact]
+        public void BeInexperiencedWhenNew()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            Assert.True(sut.IsNoob);
+
+        }
+
+        [Fact]
+        public void CalculateFullName()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            sut.FirstName = "Sarah";
+            sut.LastName = "Smith";
+
+            Assert.Equal("Sarah Smith",sut.FullName);
+        }
+    }
+}
